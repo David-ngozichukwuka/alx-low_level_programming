361c078747a5cd965bd12f2125abbc59eef6c008
David-ngozichukwuka <ngozichukwuka.david@gmail.com>
2023-03-16 14:57:45 -0700

    Printing with Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function that executes any code within its curly braces
+ *
+ * Return:always 0
+ */
+int main(void)
+{
+	puts("\" Programming is like building a multilingual puzzle");
+	return (0);
+}
