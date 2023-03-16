a8e16123286466de9620bb35ca0388bf80ec238e
David-ngozichukwuka <ngozichukwuka.david@gmail.com>
2023-03-16 15:27:39 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a function that displays code within its curly braces
+ *
+ * Return: always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
