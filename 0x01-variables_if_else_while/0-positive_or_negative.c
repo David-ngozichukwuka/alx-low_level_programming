ddec687c386daedb7932e7f22178f50e28fbb6a3
David-ngozichukwuka <ngozichukwuka.david@gmail.com>
2023-03-19 10:27:46 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,28 +3,30 @@
 #include <stdio.h>
 
 /**
- * main - Displays any code placed within its curly braces
- *
- * Return: Always 0 (Sucess)
- */
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int n;
+int n;
 
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is postive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is nrgative\n", n);
-	}
-	return (0);
+if (n > 0)
+{
+printf("%d is positive\n", n);
 }
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
+
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Writing If and else Statements
