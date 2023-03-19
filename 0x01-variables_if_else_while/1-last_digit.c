78a4c4228fc1b58d3765bf8e9e5baf7733ef8735
David-ngozichukwuka <ngozichukwuka.david@gmail.com>
2023-03-19 14:11:18 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ m = n % 10;
 
 if (m > 5)
 {
-printf("%s %d is %d and is greater than 5\n", Last_string, n , m);
+printf("%s %d is %d and is greater than 5\n", Last_string, n, m);
 }
 else if (m == 0)
 {
-printf("%s %d is %d and is 0\n", Last_string, n , m);
+printf("%s %d is %d and is 0\n", Last_string, n, m);
 }
 else if (m < 6 && m != 0)
 {
-printf("%s %d is %d and is less than 6 and not 0\n" ,Last_string, n , m);
+printf("%s %d is %d and is less than 6 and not 0\n", Last_string, n, m);
 }
 return (0);
 }
