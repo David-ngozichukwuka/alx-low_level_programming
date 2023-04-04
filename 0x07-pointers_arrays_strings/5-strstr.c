23819c372b40e4bbb3be04b1b4930fdc6803c61c
David-ngozichukwuka <ngozichukwuka.david@gmail.com>
2023-04-04 09:27:41 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - finds the first occurrenec of the substring 
+ * @haystack: main string to be scanned
+ * @needle: string to be searched within the haystack string
+ *
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (;*haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+		
+		while(*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+	}
+	return (0);
+}
