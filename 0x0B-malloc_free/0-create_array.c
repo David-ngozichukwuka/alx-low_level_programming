7db58dec621385dab4dc87867fe2dcd6e9a8b3c9
David-ngozichukwuka <ngozichukwuka.david@gmail.com>
2023-04-11 19:44:02 -0700

    Malloc Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 /**
  * create_array - create array of size size of char c
  * @size: the array size
- *
+ * @c: intial character value
  * Return: a pointer to the array
  */
 char *create_array(unsigned int size, char c)
@@ -15,10 +15,10 @@ char *create_array(unsigned int size, char c)
 	return (NULL);
 
 	string = (char *)malloc(sizeof(char) * size);
-	if (string == 0)
+	if (string == NULL)
 	return (NULL);
 
-	for (i = 0; i < size; i = 1 + 1)
+	for (i = 0; i < size; i = i + 1)
 	string[i] = c;
 	return (string);
 }
