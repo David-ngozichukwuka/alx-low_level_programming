be39e2a7d6c89324a1a853111747bed399d85830
David-ngozichukwuka <ngozichukwuka.david@gmail.com>
2023-04-21 09:02:38 -0700

    Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%.2hhx", a[x]);
+		if (x < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+}
+
+/**
+ * main - prints the opcodes of its main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+	int b;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	b = atoi(argv[1]);
+	if (b < 0)
+	{
+	printf("Error\n");
+	exit(2);
+	}
+	print_opcodes((char *)&main, b);
+	return (0);
+}
