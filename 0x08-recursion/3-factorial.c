e000aea57b150a79b425f5392b222f35795d3001
David-ngozichukwuka <ngozichukwuka.david@gmail.com>
2023-06-08 06:39:06 -0700

    Recursion Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - integer factorial
+ * @n: integer
+ * Return: integer
+ */
+int factorial(int n)
+{
+	int result;
+
+	if (n == 0)
+		return (1);
+	else if (n > 0)
+		result = n * factorial(n - 1);
+	else if (n < 0)
+	{     
+	       	return (-1);
+	}
+	return (result);
+}
+
+	
